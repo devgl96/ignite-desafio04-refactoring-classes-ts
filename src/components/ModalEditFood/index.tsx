b3bc0a9a5a921c349e550b822a7f23d456e9ab43
devgl96 <georgelucas.dev@gmail.com>
2022-01-19 15:21:29 -0300

    Updated the ModalEditFood and Challenge Completed

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from 'react';
+import { createRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
@@ -18,29 +18,31 @@ interface FoodProps {
 interface ModalComponentProps {
   isOpen: boolean;
   setIsOpen: () => void;
-  handleAddFood: (food: FoodProps) => void;
+  editingFood: FoodProps;
+  handleUpdateFood: (food: FoodProps) => void;
 };
 
-export function ModalAddFood({ setIsOpen, handleAddFood, isOpen }: ModalComponentProps) {
+export function ModalEditFood({ isOpen, setIsOpen, editingFood, handleUpdateFood }: ModalComponentProps) {
   const formRef = createRef<FormHandles>();
 
   async function handleSubmit(data: FoodProps) {
-    handleAddFood(data);
+    handleUpdateFood(data);
     setIsOpen();
   }
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" />
 
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
 
         <Input name="description" placeholder="Descrição" />
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
@@ -48,5 +50,4 @@ export function ModalAddFood({ setIsOpen, handleAddFood, isOpen }: ModalComponen
       </Form>
     </Modal>
   );
-
 }
